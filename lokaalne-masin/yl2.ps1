7f4c81ab533495171d71495dfba502c2925563a7
Kermo Nurmeoja <25592156+kermonurmeoja@users.noreply.github.com>
2023-10-04 08:53:43 +0200

    Praktikum 1-2 üleslaadimine

---
+++ lokaalne-masin/yl2.ps1
@@ -0,0 +1,22 @@
+﻿# Skript kustutab lokaalse kasutaja, küsides kasutajalt ees- ja perekonnanime.
+
+# Tsüklis küsitakse ees- ja perekonnanime, luuakse kasutajanimi eesnimi.perekonnanimi ja kontrollitakse, kas on sisestatud ladina sümbolites
+do 
+{
+    $eesnimi = Read-Host "Sisesta eesnimi "
+    $perenimi = Read-Host "Sisesta perekonnanimi "
+    $taisnimi = "$eesnimi $perenimi".ToLower()
+    $kasutajanimi = "$eesnimi.$perenimi".ToLower()
+}until($kasutajanimi -match "^[a-z\.\-]+$")
+
+Write-Host "Kustutan kasutaja :" $kasutajanimi
+
+# Kustutame kasutaja.
+Remove-LocalUser -Name $kasutajanimi -Verbose
+
+# Kontrollime, kas kasutaja kustutati edukalt
+if ($?) {
+    Write-Host "Kasutaja kustutati edukalt :" $kasutajanimi -ForegroundColor Green
+} else {
+    Write-Host "Kasutaja $kasutajanimi kustutamine ebaõnnestus. Veateade : $Error[0]"
+}
