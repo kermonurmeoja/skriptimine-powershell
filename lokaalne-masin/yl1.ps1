dce21608272547f5a2b85f371f7fe6ddd29c6064
Kermo Nurmeoja <25592156+kermonurmeoja@users.noreply.github.com>
2023-10-09 10:54:13 +0200

    Uus täisnime formaat
    Nüüd on ees- ja perekonnanime esitähed suured, sealjuures kasutajanimi on väiketähtedes.

--- lokaalne-masin/yl1.ps1
+++ lokaalne-masin/yl1.ps1
@@ -1,17 +1,23 @@
-# Skript loob lokaalse kasutaja, küsides kasutajalt ees- ja perekonnanime.
+﻿# Skript loob lokaalse kasutaja, küsides kasutajalt ees- ja perekonnanime.
 
 # Tsüklis küsitakse ees- ja perekonnanime, luuakse kasutajanimi eesnimi.perekonnanimi ja kontrollitakse, kas on sisestatud ladina sümbolites
 do 
 {
     $eesnimi = Read-Host "Sisesta eesnimi "
     $perenimi = Read-Host "Sisesta perekonnanimi "
-    $taisnimi = "$eesnimi $perenimi".ToLower()
-    $kasutajanimi = "$eesnimi.$perenimi".ToLower()
+
+    # Teeme kenama formaadi.
+    $eesnimi = $eesnimi.Substring(0, 1).ToUpper() + $eesnimi.Substring(1).ToLower()
+    $perenimi = $perenimi.Substring(0, 1).ToUpper() + $perenimi.Substring(1).ToLower()
+    $taisnimi = "$eesnimi $perenimi"
+
+    # Lisaks muudame täpitähed tavatähtedeks
+    $kasutajanimi = "$eesnimi.$perenimi".ToLower() -replace 'ä', 'a' -replace 'ö', 'o' -replace 'ü', 'u' -replace 'õ', 'o'
 }until($kasutajanimi -match "^[a-z\.\-]+$")
 
 Write-Host "Loon kasutaja :" $kasutajanimi
 
-# Loome algse parooli Parool1!
+# Loome algse parooli "Parool1!"
 $parool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
 
 # Loome kasutaja.
