006a8515230a4851690535c0e5478323333fadb4
Kermo Nurmeoja <25592156+kermonurmeoja@users.noreply.github.com>
2023-10-09 10:56:38 +0200

    Ülesanne 6 üleslaadimine

---
+++ domeen/yl6.ps1
@@ -0,0 +1,27 @@
+﻿# Skript varundab arvutis eksisteerivate kasutajate kodukataloogid kausta C:\Backup. 
+
+# Defineerime Backup kausta asukoha ning tuvastame kuupäeva
+$kaust = "C:\Backup"
+$aeg = Get-Date -Format "dd.MM.yyyy"
+
+# Tuvastame kasutajad (nii domeenis kui ka lokaalsed)
+$kasutajad = Get-LocalUser | Where-Object { $_.Enabled -eq $true }
+
+foreach ($kasutaja in $kasutajad) {
+    $kasutajanimi = $kasutaja.Name
+    $kodukaust = "C:\Users\$kasutajanimi"
+    $backupkaust = $kaust + "\" + "$kasutajanimi-$aeg.zip"
+    
+    # Kontrollime, kas antud arvutis eksisteerib kasutaja kodukaust
+    # Kui eksisteerib
+    if (Test-Path -Path $kodukaust -PathType Container) {
+        Compress-Archive -Path $kodukaust -DestinationPath $backupkaust -Force
+        Write-Host "Kasutaja $kasutajanimi kodukataloogi varundus õnnestus : $backupkaust" -ForegroundColor Green
+
+    }
+    # Kui ei eksisteeri
+    else {
+        Write-Host "Kasutaja $kasutajanimi kodukataloogi varundus ebaõnnestus : Selles arvutis ei eksisteeri $kasutajanimi kodukausta." -ForegroundColor Red
+    }
+
+}
