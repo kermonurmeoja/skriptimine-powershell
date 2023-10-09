ec98163ff7c18a97ccecf113e48111b4a06cac7c
Kermo Nurmeoja <25592156+kermonurmeoja@users.noreply.github.com>
2023-10-09 10:54:55 +0200

    Pisiparandused

--- lokaalne-masin/yl2.ps1
+++ lokaalne-masin/yl2.ps1
@@ -5,7 +5,6 @@ do
 {
     $eesnimi = Read-Host "Sisesta eesnimi "
     $perenimi = Read-Host "Sisesta perekonnanimi "
-    $taisnimi = "$eesnimi $perenimi".ToLower()
     $kasutajanimi = "$eesnimi.$perenimi".ToLower()
 }until($kasutajanimi -match "^[a-z\.\-]+$")
 
